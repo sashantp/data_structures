6862bc4a1cb94f54a0410ea1eb71a9ceab6e1fe6
sashant <sashant9@gmail.com>
2017-01-15 18:18:33 +0530

    terminal chat working code

--- chat.py
+++ chat.py
@@ -2,7 +2,7 @@ import socket
 
 HOST = ''
 PORT = 8125;
-
+data = ""
 
 try:
 
@@ -20,7 +20,9 @@ try:
 		data = conn.recv(1024)
 		print "chat" , data
 		if not data : break
-		conn.sendall(data)
+		user_input = raw_input("Please type message: or Enter to close")
+		conn.sendall(user_input)
+		if len(user_input) == 0 : break
 
 	conn.close()
 
--- chat_client.py
+++ chat_client.py
@@ -2,6 +2,7 @@ import socket
 
 HOST = '127.0.0.4'
 PORT = 8125
+data = ""
 
 try :
 
@@ -9,13 +10,14 @@ try :
 
 	s.connect((HOST,PORT))
 
-	user_input = raw_input("Please type message:")
+	while 1 :
 
-	if len(user_input) > 0:
-
-		s.sendall(user_input)
-
-		data = s.recv(1024)
+		user_input = raw_input("Please type message or Enter to close :")
+	 	if len(user_input) == 0 : break
+		if len(user_input) > 0 :
+			s.sendall(user_input)
+			data = s.recv(1024)
+			print "chat" , data
 
 	s.close()
 
